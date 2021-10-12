c2cc98479a78817d43238ce566414acf6f96bb57
appletri <tri_duc_lam@hotmail.com>
2021-10-11 23:46:36 -0600

    finalized functions, updating design

--- script.js
+++ script.js
@@ -1,35 +1,48 @@
 const library = document.querySelector(".library");
 const bookIn = document.querySelector(".book");
 const authorIn = document.querySelector(".author");
+const progress = document.querySelector(".progress")
 
 
-let myLibrary = [];
-    
- 
 
-function Book(name, author) {   
-    this.name = name;
-    this.author = author;
-    this.status = 'incomplete';
-    this.id = 0;
-    this.assignId = function() {
-        const arrayId = myLibrary.map(a => a.id);
-        randomId = getRandomInt(1000);
-        if (arrayId.includes(randomId)) {
-            this.assignId();
-        }
-        else {
-            this.id = randomId;
-        }
+let myLibrary = JSON.parse(localStorage.getItem('myLibrary')); 
+
+if (myLibrary == null) {
+    myLibrary = [];
+}
+updateLibrary();
+
+class Book {
+    constructor(name, author) {
+        this.name = name;
+        this.author = author;
+        this.status = 'incomplete';
+        this.id = 0;
+        this.assignId = function () {
+            const arrayId = myLibrary.map(a => a.id);
+            const randomId = getRandomInt(1000);
+            if (arrayId.includes(randomId)) {
+                this.assignId();
+            }
+            else {
+                this.id = randomId;
+            }
+        };
     }
 }
 
 function addBookToLibrary(){
-    let book1 = new Book(bookIn.value, authorIn.value);
-    book1.assignId();
-    myLibrary.push(book1);
-    updateLibrary();
-    console.table (myLibrary);   
+    if (bookIn.value == '' || authorIn.value == '') {
+        return;
+    }
+    else {
+        let book1 = new Book(bookIn.value, authorIn.value);
+        book1.assignId();
+        myLibrary.push(book1);
+        updateLibrary();
+        console.table (myLibrary);   
+    }
+
 }
 
 function updateLibrary() {
@@ -44,11 +57,15 @@ function updateLibrary() {
         libraryItem.innerHTML = `<p>${myLibrary[i].name} <p>${myLibrary[i].author}`;
         addToggleRead(libraryItem, targetId);
         addDelete(libraryItem, targetId);
+        
+        if (i == 0 || i % 2 == 0) {
+            libraryItem.classList.add("other-line");
+        }
         library.appendChild(libraryItem);
 
     }
- 
-     
+    localStorage.setItem('myLibrary', JSON.stringify(myLibrary));
+    updateProgress(); 
 }
 
 
@@ -83,7 +100,9 @@ function addToggleRead(parent, targetId) {
             this.textContent ='not read',
             this.classList.add('incomplete'),
             this.classList.remove('complete') 
-        )        
+        )
+        localStorage.setItem('myLibrary', JSON.stringify(myLibrary));
+        updateProgress();        
     };
     parent.appendChild(toggleRead);
 }
@@ -106,3 +125,7 @@ function getRandomInt(max) {
     return Math.floor(Math.random() * max);
 }
 
+function updateProgress() {
+    let current = myLibrary.filter(o => o.status == 'complete');
+    progress.style.width = `${(current.length/myLibrary.length)*100}%`
+}
