dd7f86f4b64437fc89739b7f5c84f11f9818d74d
appletri <tri_duc_lam@hotmail.com>
2021-10-13 21:54:31 -0600

    added ascending and descending function. added default object when no object is available

--- script.js
+++ script.js
@@ -14,8 +14,123 @@ const sortStatus = document.querySelector('#status');
 let myLibrary = JSON.parse(localStorage.getItem('myLibrary')); 
 
 
-if (myLibrary == null) {
-    myLibrary = [];
+if (myLibrary.length == 0) {
+    myLibrary = [
+        {
+          "name": "Valorant",
+          "author": "Riot Games",
+          "status": "complete",
+          "id": 314
+        },
+        {
+          "name": "Street Fighter V",
+          "author": "Capcom",
+          "status": "incomplete",
+          "id": 210
+        },
+        {
+          "name": "Rocket League",
+          "author": "Psyonix LLC",
+          "status": "complete",
+          "id": 565
+        },
+        {
+          "name": "Portal 2",
+          "author": "Valve",
+          "status": "complete",
+          "id": 6
+        },
+        {
+          "name": "Portal",
+          "author": "Valve",
+          "status": "complete",
+          "id": 126
+        },
+        {
+          "name": "Phasmophobia",
+          "author": "Kinetic Games",
+          "status": "complete",
+          "id": 799
+        },
+        {
+          "name": "Legend of Zelda",
+          "author": "Nintendo",
+          "status": "incomplete",
+          "id": 112
+        },
+        {
+          "name": "League of Legends",
+          "author": "Riot Games",
+          "status": "complete",
+          "id": 180
+        },
+        {
+          "name": "Hitman 3: Episode 5 ",
+          "author": "Epic Games",
+          "status": "incomplete",
+          "id": 464
+        },
+        {
+          "name": "Hearthstone",
+          "author": "Blizzard",
+          "status": "complete",
+          "id": 77
+        },
+        {
+          "name": "Final Fantasy XV",
+          "author": "Square Enix",
+          "status": "incomplete",
+          "id": 609
+        },
+        {
+          "name": "Final Fantasy XIII",
+          "author": "Square Enix",
+          "status": "complete",
+          "id": 84
+        },
+        {
+          "name": "Final Fantasy XI",
+          "author": "Square Enix",
+          "status": "complete",
+          "id": 834
+        },
+        {
+          "name": "Final Fantasy X",
+          "author": "Square Enix",
+          "status": "complete",
+          "id": 280
+        },
+        {
+          "name": "Final Fantasy VIII",
+          "author": "Square Enix",
+          "status": "incomplete",
+          "id": 549
+        },
+        {
+          "name": "Final Fantasy VII",
+          "author": "Square Enix",
+          "status": "incomplete",
+          "id": 29
+        },
+        {
+          "name": "Diablo 2",
+          "author": "Blizzard",
+          "status": "complete",
+          "id": 650
+        },
+        {
+          "name": "Counter Strike: Global Offensive",
+          "author": "Valve",
+          "status": "complete",
+          "id": 920
+        },
+        {
+          "name": "Chinese Cinderella",
+          "author": "Cindy Giang",
+          "status": "incomplete",
+          "id": 459
+        }
+      ];
 }
 updateLibrary();
 sortByName();
@@ -166,31 +281,64 @@ sortStatus.addEventListener('click', sortByStatus);
 
 
 function sortByName() {
-    
-
-    sortName.classList.add('blue');
     sortAuthor.classList.remove('blue');
-    sortStatus.classList.remove('blue');   
+    sortStatus.classList.remove('blue');
+    sortAuthor.classList.remove('altblue');
+    sortStatus.classList.remove('altblue');  
 
-    myLibrary.sort(function(a,b) {
-        let textA = a.name.toUpperCase();
-        let textB = b.name.toUpperCase();
-        return (textA < textB) ? -1 : (textA > textB) ? 1: 0;
-    });
+    if (sortName.className == 'blue'){
+        myLibrary.sort(function(a,b) {
+            let textA = a.name.toUpperCase();
+            let textB = b.name.toUpperCase();
+            return (textA > textB) ? -1 : (textA < textB) ? 1: 0;
+        }); 
+        sortName.classList.remove('blue');
+        sortName.classList.add('altblue');
+    }
+    else {
+        myLibrary.sort(function(a,b) {
+            let textA = a.name.toUpperCase();
+            let textB = b.name.toUpperCase();
+            return (textA < textB) ? -1 : (textA > textB) ? 1: 0;
+        });
+        sortName.classList.remove('altblue');
+        sortName.classList.add('blue');
+    }
+
+
+
+
+  
+
     updateLibrary();
 }
 
 function sortByAuthor() {
     
     sortName.classList.remove('blue');
-    sortAuthor.classList.add('blue');
     sortStatus.classList.remove('blue');
+    sortName.classList.remove('altblue');
+    sortStatus.classList.remove('altblue');
 
-    myLibrary.sort(function(a,b) {
-        let textA = a.author.toUpperCase();
-        let textB = b.author.toUpperCase();
-        return (textA < textB) ? -1 : (textA > textB) ? 1: 0;
-    });
+    if (sortAuthor.className == 'blue'){
+        myLibrary.sort(function(a,b) {
+            let textA = a.author.toUpperCase();
+            let textB = b.author.toUpperCase();
+            return (textA < textB) ? -1 : (textA > textB) ? 1: 0;
+        });
+        sortAuthor.classList.remove('blue');
+        sortAuthor.classList.add('altblue');        
+    }
+    else {
+        myLibrary.sort(function(a,b) {
+            let textA = a.author.toUpperCase();
+            let textB = b.author.toUpperCase();
+            return (textA > textB) ? -1 : (textA < textB) ? 1: 0;
+        });
+        sortAuthor.classList.remove('altblue');
+        sortAuthor.classList.add('blue');    
+    }
+
     updateLibrary();
 }
 
@@ -198,14 +346,32 @@ function sortByStatus() {
     
     sortName.classList.remove('blue');
     sortAuthor.classList.remove('blue');
-    sortStatus.classList.add('blue');
+    sortName.classList.remove('altblue');
+    sortAuthor.classList.remove('altblue');
 
-    myLibrary.sort(function(a,b) {
-        let textA = a.status;
-        let textB = b.status;
-        return (textA < textB) ? -1 : (textA > textB) ? 1: 0;
-    });
+    if (sortStatus.className == 'blue'){
+        myLibrary.sort(function(a,b) {
+            let textA = a.status;
+            let textB = b.status;
+            return (textA < textB) ? -1 : (textA > textB) ? 1: 0;
+        });
+        sortStatus.classList.remove('blue');
+        sortStatus.classList.add('altblue');    
+    }
+    else {
+        myLibrary.sort(function(a,b) {
+            let textA = a.status;
+            let textB = b.status;
+            return (textA > textB) ? -1 : (textA < textB) ? 1: 0;
+        });
+        sortStatus.classList.remove('altblue');
+        sortStatus.classList.add('blue');        
+    }
+
     updateLibrary();
 }
 
 
+function search() {
+
+}
